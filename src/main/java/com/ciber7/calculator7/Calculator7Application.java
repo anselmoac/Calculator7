7c4ba7a39651e1c387cf417c047caf9df094f9cf
anselmoac <anselmoac@cifpaviles.net>
2023-05-12 20:30:00 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber7/calculator7/Calculator7Application.java
@@ -0,0 +1,13 @@
+package com.ciber7.calculator7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber7/calculator7/Calculator7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber7.calculator7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
